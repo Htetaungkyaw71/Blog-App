f702af6234facc2e4565359b0e74ec82059fc7f7
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-08 11:16:01 +0630

    Added style for views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
+
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 end
