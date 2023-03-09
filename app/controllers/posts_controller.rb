5f7850ea36a2c0cb2350b8e04eeb8e1c2b45a5f2
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-09 15:44:27 +0630

    Added Like,Post,and comment form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :text))
+    post.author_id = current_user.id
+    if post.save
+      flash[:success] = 'New post successfully added!'
+      redirect_to user_post_url(current_user, post)
+    else
+      redirect_to new_user_post_url, notice: 'Post creation failed'
+    end
+  end
 end
